c6103b75a48ccadfc0244cc2c76cf7ac1e71fda7
阿成 <136986438+cheranlid@users.noreply.github.com>
2025-07-30 22:29:59 +0800

    Update tableview.py

--- 手势识别系统/utils/tableview.py
+++ 手势识别系统/utils/tableview.py
@@ -1,13 +1,6 @@
 # -*- coding: utf-8 -*-
 """
 =============================================
-FileName: tableview.py
-Author: 查诚
-StudentID: 20211210004
-School: 淮北师范大学
-College: 计算机科学与技术学院
-Major: 智能科学与技术专业
-Supervisor: 李晓
 Description:
     PyQt5 数据表格模型适配器，功能包括：
     1. 数据展示：将Pandas DataFrame适配到QTableView
